e363ffadc671d6c28c9071b392a9a0955d19cbc2
lankasathvika <90507761+lankasathvika@users.noreply.github.com>
2025-07-20 21:30:40 +0530

    Delete DjangoIntro directory

--- DjangoIntro/jobapplication/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class JobapplicationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'jobapplication'
--- DjangoIntro/jobapplication/forms.py
+++
@@ -1,9 +0,0 @@
-from django import  forms
-
-
-class ApplicationForm(forms.Form):
-    firstname = forms.CharField(max_length=100)
-    lastname = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    date = forms.DateField()
-    occupation = forms.CharField(max_length=100)
