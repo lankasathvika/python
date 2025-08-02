16704066933cd3d1d5655a4fc079bdf995cb84df
lankasathvika <90507761+lankasathvika@users.noreply.github.com>
2025-08-02 22:59:47 +0530

    Update main.py

--- FlaskTuitorial/main.py
+++ FlaskTuitorial/main.py
@@ -4,21 +4,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    theme = request.cookies.get('theme', 'light')
-    return render_template('tuitorial.html', theme=theme)
+    theme = request.cookies.get('theme', 'light')
+    return render_template('tuitorial.html', theme=theme)
 
 @app.route('/about/')
 def about():
-    theme = request.cookies.get('theme', 'light')
-    return render_template('about.html', theme=theme)
+    theme = request.cookies.get('theme', 'light')
+    return render_template('about.html', theme=theme)
 
 @app.route('/switch-theme/')
 def switch_theme():
-    current_theme = request.cookies.get('theme', 'light')
-    new_theme = 'dark' if current_theme == 'light' else 'light'
-    response = make_response(jsonify({'theme': new_theme}))
-    response.set_cookie('theme', new_theme)
-    return response
+    current_theme = request.cookies.get('theme', 'light')
+    new_theme = 'dark' if current_theme == 'light' else 'light'
+    response = make_response(jsonify({'theme': new_theme}))
+    response.set_cookie('theme', new_theme)
+    return response
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)
